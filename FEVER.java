bca996e8f810613f61639281f0abc26707611d95
Uzaif Ansari <114265175+uzaifansari@users.noreply.github.com>
2022-10-12 14:43:58 +0530

    Create FEVER.java

---
+++ FEVER.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    for(int i=0;i<t;i++){
+	        
+	    int X = sc.nextInt();
+		if(X>98){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	    }
+	}
+}
